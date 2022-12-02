74bac07859e54dc4379059f345c942437ff4da89
Kaspi-Chan <bozhilov53@gmail.com>
2022-12-02 15:49:08 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -192,7 +192,7 @@ namespace Copy_paste_app
         public List<TextBox> textBoxes = new List<TextBox>();
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            using (StreamWriter sw = new StreamWriter(@"D:\test.txt"))
+            using (StreamWriter sw = new StreamWriter(@"C:\test.txt"))
             {
                 foreach (TextBox text in textBoxes)
                 {
@@ -210,7 +210,7 @@ namespace Copy_paste_app
             textBoxes = temp;
             if (File.Exists(@"C:\test.txt"))
             {
-                using (StreamReader sr = new StreamReader(@"D:\test.txt"))
+                using (StreamReader sr = new StreamReader(@"C:\test.txt"))
                 {
                     for (int i = 0; i < textBoxes.Count; i++)
                     {
