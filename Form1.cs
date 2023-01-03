2fb37a33ec18ce75e66a34a9926bb730b49ed960
Kaspi-Chan <bozhilov53@gmail.com>
2023-01-03 13:08:53 +0200

    Fix Month bug, when there isn't a 0 in the input

--- Form1.cs
+++ Form1.cs
@@ -147,6 +147,8 @@ namespace Copy_paste_app
                 inputDate = inputDate.Replace('/', '-');
                 if (inputDate[1] == '-') // for dates that are before the 10th of the month e.g. 1/12/2022
                     inputDate = inputDate.Insert(0, "0");
+                if (inputDate[4] == '-')
+                    inputDate = inputDate.Insert(3, "0"); // for months that are only 1 symbol long , Jan, Feb .... e.g. 04/01/2023
                 DateTime date = DateTime.ParseExact(inputDate, "dd-MM-yyyy HH:mm",
                     System.Globalization.CultureInfo.InvariantCulture);
                 //change the hour to be -1h and 15 minutes from the input hour
